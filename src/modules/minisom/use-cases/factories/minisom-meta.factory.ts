bb6292728e95d52fe45ba31ff5ec14c6b6ffb50d
ryanx3 <ryangabriel_cap@hotmail.com>
2026-03-03 17:58:22 +0000

    tirando queue

--- src/modules/minisom/use-cases/factories/minisom-meta.factory.ts
+++ src/modules/minisom/use-cases/factories/minisom-meta.factory.ts
@@ -1,10 +1,16 @@
+import { altitudeAuthService } from "../../../../shared/infra/providers/altitude/auth.service";
+import { AltitudeCreateContact } from "../../../../shared/infra/providers/altitude/create-contact.service";
 import { MssqlMinisomRepository } from "../../repositories/mssql-minisom.repository";
 import { MinisomMetaUseCase } from "../meta/minisom-meta.use-case";
 import { MinisomMetaUploadContactsUseCase } from "../meta/upload-contacts.use-case";
 
 export function makeMinisomMetaUseCase() {
+  const altitudeCreateContact = new AltitudeCreateContact(altitudeAuthService);
   const minisomRepository = new MssqlMinisomRepository();
-  const minisomMetaUploadContacts = new MinisomMetaUploadContactsUseCase();
+  const minisomMetaUploadContacts = new MinisomMetaUploadContactsUseCase(
+    minisomRepository,
+    altitudeCreateContact,
+  );
   const minisomMetaUseCase = new MinisomMetaUseCase(
     minisomRepository,
     minisomMetaUploadContacts,
