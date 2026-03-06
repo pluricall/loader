99a0784be37062d8d969159949ca4d465cec2e9f
ryanx3 <ryangabriel_cap@hotmail.com>
2026-03-06 10:26:05 +0000

    alterando para aceitar null na endesa

--- src/modules/endesa/http/schemas/endesa-22071.schema.ts
+++ src/modules/endesa/http/schemas/endesa-22071.schema.ts
@@ -4,9 +4,18 @@ export const endesa22071Schema = z.object({
   lead_id: z.string().min(1, "lead_id obrigatório"),
   telefone: z.string().min(1, "Telefone obrigatório"),
   nome: z.string().min(1, "Nome obrigatório"),
-  apelido: z.string().optional().default(""),
-  genero: z.string().optional().default(""),
-  data_nascimento: z.string().optional().default(""),
+  apelido: z
+    .string()
+    .nullish()
+    .transform((val) => val ?? ""),
+  genero: z
+    .string()
+    .nullish()
+    .transform((val) => val ?? ""),
+  data_nascimento: z
+    .string()
+    .nullish()
+    .transform((val) => val ?? ""),
   cp4: z.coerce.number({ invalid_type_error: "cp4 deve ser número" }),
   cp3: z.coerce.number({ invalid_type_error: "cp3 deve ser número" }),
 });
