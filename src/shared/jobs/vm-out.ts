9a19f80ff0d229cdc53739a32cf1a35ad8d42672
Ryan Martins <ryangabriel_cap@hotmail.com>
2026-04-14 17:04:06 +0100

    feat: colocando pra rodar de 01h as 23h50

--- src/shared/jobs/vm-out.ts
+++ src/shared/jobs/vm-out.ts
@@ -3,12 +3,21 @@ import { makeVmOutUseCase } from "../../modules/vm-out/use-cases/factories/vm-ou
 
 export function vmOutCron() {
   const vmOutUseCase = makeVmOutUseCase();
-  cron.schedule("*/10 1-21 * * *", async () => {
-    try {
-      await vmOutUseCase.execute();
-      console.log("Cron job VM OUT finalizado");
-    } catch (err) {
-      console.error("Erro no cron job VM OUT:", err);
+
+  cron.schedule("*/10 * * * *", async () => {
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+
+    // ❌ BLOQUEIA de 23:50 até 00:59
+    if (
+      (hour === 23 && minute >= 50) ||
+      hour === 0 // tudo de 00:00 até 00:59
+    ) {
+      return;
     }
+
+    await vmOutUseCase.execute();
+    console.log("Cron VM OUT executado");
   });
 }
