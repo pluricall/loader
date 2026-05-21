6afbffb0f52b3cd9066b353dfb39ce7b26fe25b2
Ryan Martins <ryangabriel_cap@hotmail.com>
2026-05-21 16:02:58 +0100

    fix: colocando processos async

--- src/shared/jobs/plenitude-records.ts
+++ src/shared/jobs/plenitude-records.ts
@@ -4,12 +4,16 @@ import { makePlenitudeRecordingsUseCase } from "../../modules/plenitude/applicat
 const useCase = makePlenitudeRecordingsUseCase();
 
 export async function plenitudeRecordingsJob() {
-  cron.schedule("0 7 * * *", () => {
+  cron.schedule("0 7 * * *", async () => {
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
-    const dateStr = yesterday.toISOString().split("T")[0];
 
-    useCase.execute({
+    const year = yesterday.getFullYear();
+    const month = String(yesterday.getMonth() + 1).padStart(2, "0");
+    const day = String(yesterday.getDate()).padStart(2, "0");
+
+    const dateStr = `${year}-${month}-${day}`;
+    await useCase.execute({
       initialDate: dateStr,
       endDate: dateStr,
     });
